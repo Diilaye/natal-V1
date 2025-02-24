3d345449b9889ff8e99204a0f94ae298c8413bfa
Issa laye KANE <issa.k@deally.fr>
2025-02-24 00:23:28 +0000

    add news  ersion

--- src/CommonComponent/SessionWrapper.tsx
+++ src/CommonComponent/SessionWrapper.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 
 const SessionWrapper = ({children,session}:{children:React.ReactNode,session: any;}) => {
   return (
-    <SessionProvider session={session}>{children}</SessionProvider>
+    <>{children}</>
   )
 }
 
